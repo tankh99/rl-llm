2dc06cbedea3ddc0e57dcc8e0481d7793550b352
tankh99 <khanghou99@gmail.com>
2024-11-08 09:57:32 +0800

    fix: model name

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ fourbit_models = [
 ] # More models at https://huggingface.co/unsloth
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/Meta-Llama-3.2-3B",
+    model_name = "unsloth/Meta-Llama-3.2-3B-Instruct",
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit,
